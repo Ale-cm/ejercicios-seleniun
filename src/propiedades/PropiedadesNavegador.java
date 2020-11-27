c7e6e4fd176e2b8ed4b74c28d5a71021aceb8b1a
alejandro <54691090+Ale-cm@users.noreply.github.com>
2020-11-27 00:24:12 -0300

    pantalla
    modifico la mantalla dandole un maximo y luego un minimimo

--- src/propiedades/PropiedadesNavegador.java
+++ src/propiedades/PropiedadesNavegador.java
@@ -1,5 +1,6 @@
 package propiedades;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,12 +14,19 @@ public class PropiedadesNavegador {
         ChromeOptions chromeOptions = new ChromeOptions();
         //elijo la posicion de la ventana
         chromeOptions.addArguments("--window-position=850,0");
-
+        //cambio el tamaño
+        chromeOptions.addArguments("window-size=800,500");
             WebDriver driverCd = new ChromeDriver(chromeOptions);
 
 
             driverCd.get("https://es.wikipedia.org/wiki/Wikipedia:Portada");
 
-        }
+            driverCd.manage().window().maximize(); //ocupa toda la pantalla
+
+        // minimiza la pantalla 
+        Dimension dimension = new Dimension(100,100);
+
+        driverCd.manage().window().setSize(dimension);
+    }
     }
 
